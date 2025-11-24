2d42b590cafb7608eea6df6f7b7b04e84011d3c3
Pavel Alekseev <pa.alekseev@okko.tv>
2025-11-25 00:53:20 +0300

    Initial Commit

---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by pa.alekseev on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
