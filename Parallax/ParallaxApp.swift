8683e9ecd76f1e0404307e2015c64f05188d4c3e
Pavel Alekseev <pa.alekseev@okko.tv>
2025-11-27 00:01:16 +0300

    3D Particles effect

--- Parallax/ParallaxApp.swift
+++ Parallax/ParallaxApp.swift
@@ -6,6 +6,87 @@
 //
 
 import SwiftUI
+import Combine
+
+struct Particle {
+    var x: Double
+    var y: Double
+    var z: Double
+    var size: Double
+    var opacity: Double
+
+    static func random() -> Particle {
+        let theta = Double.random(in: -Double.pi...Double.pi)
+        let phi = Double.random(in: -Double.pi...Double.pi)
+        let distance = Double.random(in: 0.3...0.8)
+
+        return Particle(
+            x: sin(theta) * cos(phi) * distance,
+            y: sin(theta) * sin(phi) * distance,
+            z: cos(theta) * distance,
+            size: Double.random(in: 2...15),
+            opacity: Double.random(in: 0.5...1)
+        )
+    }
+}
+
+struct ParticlesView: View {
+    let yaw: Double
+    let pitch: Double
+
+    @State private var points = (0...1000).map { _ in Particle.random() }
+
+    var body: some View {
+        let points = points
+        Canvas { context, size in
+            for index in 0..<points.count {
+                let point = points[index]
+
+                // yaw
+                let x1 = cos(yaw) * point.x + sin(yaw) * point.z
+                let y1 = point.y
+                let z1 = -sin(yaw) * point.x + cos(yaw) * point.z
+
+                // pitch
+                let x = x1
+                let y = cos(pitch) * y1 - sin(pitch) * z1
+                let z = sin(pitch) * y1 + cos(pitch) * z1
+
+                let particleSize: Double = point.size + z
+
+                let rect = CGRect(
+                    origin: CGPoint(
+                        x: (x + 1.0)/2.0 * size.width - particleSize/2.0,
+                        y: (y + 1.0)/2.0 * size.height - particleSize/2.0
+                    ),
+                    size: CGSize(
+                        width: particleSize,
+                        height: particleSize
+                    ))
+                let opacity = point.opacity * (z + 1.0)/2.0
+                context.fill(Circle().path(in: rect), with: .color(.white.opacity(opacity)))
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var yaw = 0.0
+    @State private var pitch = 0.0
+
+    var body: some View {
+        VStack {
+            ParticlesView(yaw: yaw, pitch: pitch)
+                .ignoresSafeArea()
+                .aspectRatio(1, contentMode: .fill)
+
+            Slider(value: $yaw, in: -Double.pi...Double.pi).padding(.horizontal)
+            Slider(value: $pitch, in: -Double.pi...Double.pi).padding(.horizontal)
+        }
+        .background(.black)
+    }
+}
+
 
 @main
 struct ParallaxApp: App {
@@ -14,4 +95,8 @@ struct ParallaxApp: App {
             ContentView()
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
