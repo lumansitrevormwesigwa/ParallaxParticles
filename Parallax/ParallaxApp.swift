91a4073c9acdd12d560ebaf20484db1997ad8d64
Pavel Alekseev <pa.alekseev@okko.tv>
2025-11-27 00:01:58 +0300

    Wiggle particles a little bit

--- Parallax/ParallaxApp.swift
+++ Parallax/ParallaxApp.swift
@@ -14,6 +14,8 @@ struct Particle {
     var z: Double
     var size: Double
     var opacity: Double
+    var wiggleAmount: Double
+    var wiggleSpeed: Double
 
     static func random() -> Particle {
         let theta = Double.random(in: -Double.pi...Double.pi)
@@ -25,7 +27,9 @@ struct Particle {
             y: sin(theta) * sin(phi) * distance,
             z: cos(theta) * distance,
             size: Double.random(in: 2...15),
-            opacity: Double.random(in: 0.5...1)
+            opacity: Double.random(in: 0.5...1),
+            wiggleAmount: Double.random(in: 0.01...0.2),
+            wiggleSpeed: Double.random(in: 0.0...0.2)
         )
     }
 }
@@ -37,34 +41,44 @@ struct ParticlesView: View {
     @State private var points = (0...1000).map { _ in Particle.random() }
 
     var body: some View {
-        let points = points
-        Canvas { context, size in
-            for index in 0..<points.count {
-                let point = points[index]
+        TimelineView(.animation) { timeline in
+            let points = points
+            let time = timeline.date.timeIntervalSinceReferenceDate
 
-                // yaw
-                let x1 = cos(yaw) * point.x + sin(yaw) * point.z
-                let y1 = point.y
-                let z1 = -sin(yaw) * point.x + cos(yaw) * point.z
+            Canvas { context, size in
+                for index in 0..<points.count {
+                    let point = points[index]
 
-                // pitch
-                let x = x1
-                let y = cos(pitch) * y1 - sin(pitch) * z1
-                let z = sin(pitch) * y1 + cos(pitch) * z1
+                    // yaw
+                    let x1 = cos(yaw) * point.x + sin(yaw) * point.z
+                    let y1 = point.y
+                    let z1 = -sin(yaw) * point.x + cos(yaw) * point.z
 
-                let particleSize: Double = point.size + z
+                    // pitch
+                    var x = x1
+                    var y = cos(pitch) * y1 - sin(pitch) * z1
+                    var z = sin(pitch) * y1 + cos(pitch) * z1
 
-                let rect = CGRect(
-                    origin: CGPoint(
-                        x: (x + 1.0)/2.0 * size.width - particleSize/2.0,
-                        y: (y + 1.0)/2.0 * size.height - particleSize/2.0
-                    ),
-                    size: CGSize(
-                        width: particleSize,
-                        height: particleSize
-                    ))
-                let opacity = point.opacity * (z + 1.0)/2.0
-                context.fill(Circle().path(in: rect), with: .color(.white.opacity(opacity)))
+                    // wiggle
+                    let offset = time * point.wiggleSpeed + Double(index)
+                    x += sin(offset) * point.wiggleAmount
+                    y += cos(offset) * point.wiggleAmount
+                    z += cos(offset) * point.wiggleAmount
+
+                    let particleSize: Double = point.size + z
+
+                    let rect = CGRect(
+                        origin: CGPoint(
+                            x: (x + 1.0)/2.0 * size.width - particleSize/2.0,
+                            y: (y + 1.0)/2.0 * size.height - particleSize/2.0
+                        ),
+                        size: CGSize(
+                            width: particleSize,
+                            height: particleSize
+                        ))
+                    let opacity = point.opacity * (z + 1.0)/2.0
+                    context.fill(Circle().path(in: rect), with: .color(.white.opacity(opacity)))
+                }
             }
         }
     }
